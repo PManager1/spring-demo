7f541d9f2b5985297b8b2ec6bb3d56cca5c38992
jpca999 <jpca999@gmail.com>
2018-06-18 18:01:25 -0700

    before 28

---
+++ src/com/luv2code/springdemo/BaseBallCoach.java
@@ -0,0 +1,10 @@
+package com.luv2code.springdemo;
+
+public class BaseBallCoach implements Coach{
+
+	@Override
+	public String getDailyWorkout () {
+		return "--BaseBallCoach -  Do 30 - pushups";
+	}
+	
+}
---
+++ src/com/luv2code/springdemo/MyApp.java
@@ -0,0 +1,20 @@
+package com.luv2code.springdemo;
+
+public class MyApp {
+
+	public static void main(String[] args) {
+		
+		System.out.println(" main calling in MyApp ");
+		
+//	BaseBallCoach c1 = new BaseBallCoach();  // old way but we need to do it with Interface.
+//	Now this one is from Interface 
+//		Coach c1 = new BaseBallCoach(); 
+		
+		Coach c1 = new TrackCoach(); 
+		
+		System.out.println("  spitting method +  " +  c1.getDailyWorkout() );
+		
+
+	}
+
+}
