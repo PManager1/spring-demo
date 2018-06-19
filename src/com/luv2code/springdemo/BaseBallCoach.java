450194a0332ae1239e7837c44f996fcf71ad4a68
jpca999 <jpca999@gmail.com>
2018-06-18 21:14:25 -0700

    till 28 workign fine

--- src/com/luv2code/springdemo/BaseBallCoach.java
+++ src/com/luv2code/springdemo/BaseBallCoach.java
@@ -1,10 +1,24 @@
 package com.luv2code.springdemo;
 
 public class BaseBallCoach implements Coach{
+	
+	private FortuneService fortuneService; 
+	
+	public BaseBallCoach( FortuneService theFortuneService ) {
+		setFortuneService(theFortuneService); 
+	}
 
 	@Override
 	public String getDailyWorkout () {
 		return "--BaseBallCoach -  Do 30 - pushups";
+	}
+
+	public FortuneService getFortuneService() {
+		return fortuneService;
+	}
+
+	public void setFortuneService(FortuneService fortuneService) {
+		this.fortuneService = fortuneService;
 	}
 	
 }
